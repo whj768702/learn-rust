3bacaf813dfe1eee29ea4ce14cec96831634317f
whj768702 <whj768702@gmail.com>
2022-12-13 17:32:37 +0800

    feat(rust): define a fn

--- world_hello/src/main.rs
+++ world_hello/src/main.rs
@@ -1,3 +1,12 @@
+fn greet_world() {
+    let chinese = "世界，你好";
+    let english = "World, hello.";
+    let regions = [chinese, english];
+
+    for region in regions.iter() {
+        println!("{}", &region);
+    }
+}
 fn main() {
-    println!("Hello, world!");
+    greet_world();
 }
