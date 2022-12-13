4a31205ffa63711adad4ea23ab5f7508ebd40a69
whj768702 <whj768702@gmail.com>
2022-12-13 17:41:42 +0800

    feat(rust): 循环和控制

--- world_hello/src/main.rs
+++ world_hello/src/main.rs
@@ -1,12 +1,27 @@
-fn greet_world() {
-    let chinese = "世界，你好";
-    let english = "World, hello.";
-    let regions = [chinese, english];
-
-    for region in regions.iter() {
-        println!("{}", &region);
-    }
-}
 fn main() {
-    greet_world();
+    let penguin_data = "\
+    common name, length (cm)
+    Little penguin, 33
+    Yellow-eyed penguin, 65
+    Fiordland penguin, 60
+    Invalid, data";
+
+    let records = penguin_data.lines();
+
+    for (i, record) in records.enumerate() {
+        if i == 0 || record.trim().len() == 0 {
+            continue;
+        }
+
+        let fields: Vec<_> = record.split(",").map(|field| field.trim()).collect();
+
+        if cfg!(debug_assertions) {
+            eprint!("debug: {:?} -> {:?}", record, fields);
+        }
+
+        let name = fields[0].to_string();
+        if let Ok(length) = fields[1].parse::<f32>() {
+            println!("{}, {}cm", name, length);
+        }
+    }
 }
