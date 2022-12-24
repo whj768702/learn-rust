2393b4003979de3581cc13bc7e75f91872f07ff6
whj768702 <whj768702@gmail.com>
2022-12-24 13:58:25 +0800

    feat(rust): browser engine

--- browser_engine/src/main.rs
+++ browser_engine/src/main.rs
@@ -1,3 +1,35 @@
+extern crate getopts;
+extern crate image;
+
+use std::fs::File;
+use std::io::Read;
+
+pub mod dom;
+pub mod html;
+
 fn main() {
-    println!("Hello, world!");
+    let mut opts = getopts::Options::new();
+    opts.optopt("h", "html", "HTML document", "FILENAME");
+    opts.optopt("c", "css", "CSS stylesheet", "FILENAME");
+    opts.optopt("o", "output", "Output file", "FILENAME");
+    opts.optopt("f", "format", "Output file format", "png | pdf");
+
+    let matches = opts.parse(std::env::args().skip(1)).unwrap();
+    let str_arg = |flag: &str, default: &str| -> String {
+        matches.opt_str(flag).unwrap_or(default.to_string())
+    };
+    let html = read_source(str_arg("h", "examples/test.html"));
+    println!("{}", html);
+
+    let root_node = html::parse(html);
+    println!("{:?}", root_node);
+}
+
+fn read_source(filename: String) -> String {
+    let mut str = String::new();
+    File::open(filename)
+        .unwrap()
+        .read_to_string(&mut str)
+        .unwrap();
+    str
 }
