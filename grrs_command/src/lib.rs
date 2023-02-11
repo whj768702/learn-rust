88b8742b872a6c8f27aa3511f581ef5dfba37bee
whj768702 <whj768702@gmail.com>
2023-02-11 17:47:20 +0800

    feat(rust): 替换anyhow包，删除注释代码

--- grrs_command/src/lib.rs
+++ grrs_command/src/lib.rs
@@ -1,3 +1,5 @@
+#![allow(unused)]
+
 pub fn find_matches(content: &str, pattern: &str, mut writer: impl std::io::Write) {
     for line in content.lines() {
         if line.contains(pattern) {
--- grrs_command/src/main.rs
+++ grrs_command/src/main.rs
@@ -1,7 +1,8 @@
 #![allow(unused)]
-use anyhow::{Context, Result};
+use std::fmt::Error;
+
+use anyhow_ext::{Context, Result};
 use clap::Parser;
-use log::{info, warn};
 
 mod test;
 
@@ -14,17 +15,11 @@ struct Cli {
 }
 
 fn main() -> Result<()> {
-    // env_logger::init();
-    // info!("starting up");
-    // warn!("oops, nothing implemented!");
     let args = Cli::parse();
 
-    let content = std::fs::read_to_string(&args.path).expect("Could not read file");
-    // let content = std::fs::read_to_string(&args.path)
-    //     .with_context(|| format!("could not read file `{}`", args.path.display()))?;
     // 获取文件内容
-    // let content = std::fs::read_to_string(&args.path)
-    //     .with_context(|| format!("Could not read file '{}'", args.path.display()))?;
+    let content = std::fs::read_to_string(&args.path)
+        .with_context(|| format!("Could not read file '{}'", args.path.display()))?;
 
     grrs_command::find_matches(&content, &args.pattern, &mut std::io::stdout());
     Ok(())
