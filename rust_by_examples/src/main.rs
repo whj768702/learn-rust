0ac9ba33268e1641552ca99a77a77e7943a6b819
whj768702 <whj768702@gmail.com>
2023-08-16 09:51:38 +0800

    refactor(rust_by_example): 拆分模块

--- rust_by_examples/src/main.rs
+++ rust_by_examples/src/main.rs
@@ -1,63 +1,7 @@
-use std::fmt;
+pub mod hello_word;
 
-#[derive(Debug)]
-struct MinMax(i64, i64);
-
-#[derive(Debug)]
-struct Point2D {
-    x: f64,
-    y: f64,
-}
-
-#[derive(Debug)]
-struct Complex {
-    real: f64,
-    imag: f64,
-}
-
-impl fmt::Display for MinMax {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "({}, {})", self.0, self.1)
-    }
-}
-impl fmt::Display for Point2D {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "x: {}, y: {}", self.x, self.y)
-    }
-}
-
-impl fmt::Display for Complex {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{} + {}i", self.real, self.imag)
-    }
-}
+use hello_word::hello_world_print;
 
 fn main() {
-    let minmax = MinMax(0, 14);
-
-    println!("Compare structures:");
-    println!("Display: {}", minmax);
-    println!("Debug: {:?}", minmax);
-
-    let big_range = MinMax(-300, 300);
-    let small_range = MinMax(-3, 3);
-
-    println!(
-        "The big range is {big} and the small is {small}",
-        small = small_range,
-        big = big_range
-    );
-
-    let point = Point2D { x: 3.3, y: 7.2 };
-    println!("Compare points:");
-    println!("Display: {}", point);
-    println!("Debug: {:?}", point);
-
-    let complex = Complex {
-        real: 3.3,
-        imag: 7.2,
-    };
-    println!("Compare complex:");
-    println!("Display: {}", complex);
-    println!("Debug: {:?}", complex);
+    hello_world_print();
 }
