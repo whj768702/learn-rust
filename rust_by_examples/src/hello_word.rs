84a74b9ca5780db26f35beb9e4152c89fd8cebfd
whj768702 <whj768702@gmail.com>
2023-09-21 17:14:07 +0800

    feat(rust_by_example): List输出

--- rust_by_examples/src/hello_word.rs
+++ rust_by_examples/src/hello_word.rs
@@ -15,6 +15,8 @@ struct Complex {
     imag: f64,
 }
 
+struct List(Vec<i32>);
+
 impl fmt::Display for MinMax {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "({}, {})", self.0, self.1)
@@ -29,6 +31,23 @@ impl fmt::Display for Point2D {
 impl fmt::Display for Complex {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "{} + {}i", self.real, self.imag)
+    }
+}
+
+impl fmt::Display for List {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let vec = &self.0;
+
+        write!(f, "[")?;
+
+        for (count, v) in vec.iter().enumerate() {
+            if count != 0 {
+                write!(f, ", ")?;
+            }
+            write!(f, "{}", v)?;
+        }
+
+        write!(f, "]")
     }
 }
 
@@ -60,4 +79,7 @@ pub fn hello_world_print() {
     println!("Compare complex:");
     println!("Display: {}", complex);
     println!("Debug: {:?}", complex);
+
+    let v = List(vec![1, 2, 3]);
+    println!("Display List: {}", v);
 }
