739fd000468503e4b8d012e68dadee4fd63d5ab6
whj768702 <whj768702@gmail.com>
2024-02-27 09:07:31 +0800

    feat(rust_by_example): 枚举示例

--- rust_by_examples/src/enum_demo.rs
+++ rust_by_examples/src/enum_demo.rs
@@ -11,8 +11,23 @@ fn inspect(event: WebEvent) {
         WebEvent::PageLoad => println!("page loaded"),
         WebEvent::PageUnload => println!("page unloaded"),
         WebEvent::KeyPress(c) => println!("pressed '{}'", c),
+        WebEvent::Paste(s) => println!("pasted \"{}\"", s),
         WebEvent::Click { x, y } => {
             println!("clicked at x={}, y={}", x, y);
         }
     }
+}
+
+pub fn enum_inspect() {
+    let pressed = WebEvent::KeyPress('x');
+    let pasted = WebEvent::Paste("my text".to_owned());
+    let click = WebEvent::Click { x: 20, y: 80 };
+    let load = WebEvent::PageLoad;
+    let unload = WebEvent::PageUnload;
+
+    inspect(pressed);
+    inspect(pasted);
+    inspect(click);
+    inspect(load);
+    inspect(unload);
 }
--- rust_by_examples/src/main.rs
+++ rust_by_examples/src/main.rs
@@ -1,12 +1,17 @@
+pub mod enum_demo;
 pub mod hello_word;
 pub mod primitives;
 pub mod structures;
 
+use enum_demo::enum_inspect;
 use hello_word::hello_world_print;
 
 fn main() {
+    println!("enum print starts");
     hello_world_print();
     primitives::primitives_fn();
     primitives::primitives_activity();
     structures::structs_fn();
+    enum_inspect();
+    println!("enum print ends");
 }
